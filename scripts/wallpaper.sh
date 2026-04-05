b5e932e4827dd56889821d0c4b2c7e0d139bcac5
Stefan Lapointe <stefan.lapointe@mail.mcgill.ca>
2026-04-04 23:09:08 -0400

    Reconfigure i3 to handle two monitors

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Get screen resolution (requires xrandr)
-SCREEN_W=$(xrandr --current | grep '*' | awk '{print $1}' | cut -d'x' -f1 | head -1)
-SCREEN_H=$(xrandr --current | grep '*' | awk '{print $1}' | cut -d'x' -f2 | head -1)
+# Using the maximum is correct for multi-monitor setups as it prevents
+# images from ever being stretched.
+MAX_SCREEN_W=$(xrandr --current | grep '*' | awk '{print $1}' | cut -d'x' -f1 | sort -n | tail -1)
+MAX_SCREEN_H=$(xrandr --current | grep '*' | awk '{print $1}' | cut -d'x' -f2 | sort -n | tail -1)
 
 # Choose a random wallpaper
 IMAGE=$(find ~/Pictures/wallpapers -type f -print0 | shuf -z -n 1)
@@ -21,7 +23,7 @@ IMG_W=$(identify -format "%w" "$IMAGE")
 IMG_H=$(identify -format "%h" "$IMAGE")
 
 # If image is larger than screen in both dimensions, scale it down to fill
-if [ "$IMG_W" -gt "$SCREEN_W" ] && [ "$IMG_H" -gt "$SCREEN_H" ]; then
+if [ "$IMG_W" -gt "$MAX_SCREEN_W" ] && [ "$IMG_H" -gt "$MAX_SCREEN_H" ]; then
     feh --bg-fill "$IMAGE"
     exit 0
 fi
@@ -30,7 +32,7 @@ fi
 COLOUR=$(magick "$IMAGE" -kmeans 2 -unique-colors -define ftxt:format='\H\n' ftxt:- | head -n 1)
 
 # If image is larger than screen in one dimension, scale it down to fit
-if [ "$IMG_W" -gt "$SCREEN_W" ] || [ "$IMG_H" -gt "$SCREEN_H" ]; then
+if [ "$IMG_W" -gt "$MAX_SCREEN_W" ] || [ "$IMG_H" -gt "$MAX_SCREEN_H" ]; then
     feh --image-bg "$COLOUR" --bg-max "$IMAGE"
 else
     feh --image-bg "$COLOUR" --bg-center "$IMAGE"
